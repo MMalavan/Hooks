8f3b7e390c0bd894620aa2ffbd25b7f42d10fda3
MMalavan <159991875+MMalavan@users.noreply.github.com>
2024-02-17 15:06:49 +0530

    projects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+ import React,{useState} from 'react'
+ import backgroundImage from './nature-background.jpg'; 
 
-function App() {
+export default function App() {
+    const [id]=useState(101)
+    const [count,setcount]=useState(0);
+    const [name]=useState('Malavan');
+    const [salary]=useState([10000]);
+    const [Address,setaddress]=useState({Street:'South Street',village:'Srimushnam',district:''});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div >
+       <style>
+        {`
+          body {
+            background-image: url(${backgroundImage});
+            background-size: cover;
+            background-position: center;
+            height: 100vh;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+        `}
+      </style>
+      
+       <div>Employee_Id :{id}</div>
+      <div>Name :{name}</div> 
+      
+      <div>Salary:{salary}</div>
 
-export default App;
+      <div>Address:{' '+Address.Street +','+Address.village+','+Address.district}</div>
+      <input type="text" value={Address.district} onChange={(e) =>{
+        setaddress({...Address,district: e.target.value})
+      }}></input>
+      <div>No of Days Working :{count} </div>
+      <div>
+        <button onClick={()=>{
+            setcount(count+1);
+        }}>
+            Present
+        </button>{'     '}
+        <button onClick={()=>{
+            setcount(count-1);
+        }}>
+            Absent
+        </button>
+      </div>
+      
+      <div>
+        
+      </div>
+          
+
+    </div>
+  )
+}
